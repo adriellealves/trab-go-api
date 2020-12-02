cbf6be955bc77295036def0b67862406cb7d3959
Adrielle <dricadriadrielle.alves43@gmail.com>
2020-12-02 14:26:39 -0300

    modicacoes em database

--- database/Connection.go
+++ database/Connection.go
@@ -1,12 +1,12 @@
 package database
 
-import(
+import (
 	"database/sql"
 	"fmt"
-	_"github.com/go-sql-driver/mysql"
 )
+
 func SetConnection() *sql.DB {
-	connectionDB, err := sql.Open("mysql", "root:@/go_api")
+	connectionDB, err := sql.Open("mysql", "root:./go_api")
 
 	if err != nil {
 		fmt.Println("Error to connect", err)
